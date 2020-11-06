2557dab5a6771e8c7d68cdd3170cf501cdcda1b7
Jennifer Cant <jcant6@gmail.com>
2020-11-06 18:07:16 +0000

    fitness decrease and hunger increase with grow up funciton

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -4,11 +4,44 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-});
 
-describe('constructor', () => {
     it('sets the name property', () => {
         const pet = new Pet ('Fido');
         expect(pet.name).toEqual('Fido');
     });
+
+    it('has an initial age of 0', () => {
+        const pet = new Pet ('Fido');
+        expect(pet.age).toEqual(0);
+    });
+
+    it('has an initial hunger of 0', () => {
+        const pet = new Pet ('Fido');
+        expect(pet.hunger).toBe(0);
+    })
+
+    it('has an initial fitness of 10', () => {
+        const pet = new Pet ('Fido');
+        expect(pet.fitness).toBe(10);
+    });
 });
+
+describe('growUp', () => {
+    it('increments age by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
+    it('increments hunger by 5', () => {
+        const pet = new Pet ('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+    it('decreases fitness by 3', () => {
+        const pet = new Pet ('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    })
+
+})
+
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,17 @@
-function Pet(name) {
+function Pet(name){
     this.name = name;
+    this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
+};
+
+Pet.prototype = {
+    constructor: Pet,
+    growUp: function() {
+    this.age ++;
+    this.hunger += 5;
+    this.fitness -= 3;
 }
+};
 
 module.exports = Pet;
